dd88fb706024ac9c09bc9f0cfeef0f561b0785d2
Mark Bellott <mdbellott@gmail.com>
2013-03-07 12:53:27 -0500

    Added Patient Class

---
+++ TRx/Patient.h
@@ -0,0 +1,28 @@
+//
+//  Patient.h
+//  TRx
+//
+//  Created by Mark Bellott on 3/7/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum{
+    CATARACT,
+    HERNIA
+} complaint;
+
+@interface Patient : NSObject{
+    NSString *name;
+    complaint chiefComplaint;
+    UIImage *photoID;
+}
+
+@property(nonatomic,retain) NSString *name;
+@property(nonatomic, readwrite) complaint chiefComplaint;
+@property(nonatomic, retain) UIImage *photoID;
+
+-(id)initWithName:(NSString *)n ChiefComplaint:(complaint)c PhotoID:(UIImage*)p;
+
+@end
