90107611dbebd39e1dfdfd375b24ede324fdb892
John Cotham <jcotham@utk.edu>
2013-03-10 21:17:39 -0400

    Merge remote-tracking branch 'origin'

---
+++ TRx/HistoryNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  HistoryNavigationController.m
+//  TRx
+//
+//  Created by Mark Bellott on 3/10/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import "HistoryNavigationController.h"
+
+@interface HistoryNavigationController ()
+
+@end
+
+@implementation HistoryNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
