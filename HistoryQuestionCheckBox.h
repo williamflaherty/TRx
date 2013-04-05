05d334a7607fae4453db1713af4263aef091106b
Mark Bellott <mdbellott@gmail.com>
2013-04-05 10:23:39 -0400

    HistoryQuestions Classes

---
+++ HistoryQuestionCheckBox.h
@@ -0,0 +1,13 @@
+//
+//  HistoryQuestionCheckBox.h
+//  TRx
+//
+//  Created by Mark Bellott on 4/5/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryQuestionCheckBox : UIButton
+
+@end
---
+++ HistoryQuestionSelector.h
@@ -0,0 +1,13 @@
+//
+//  HistoryQuestionSelector.h
+//  TRx
+//
+//  Created by Mark Bellott on 4/5/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryQuestionSelector : UISegmentedControl
+
+@end
