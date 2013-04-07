7fc79c85a3fd90121ad235f4a7a4312e0b30f234
Mark Bellott <mdbellott@gmail.com>
2013-04-07 14:18:31 -0400

    rs

---
+++ HQTextField.h
@@ -0,0 +1,13 @@
+//
+//  HQTextField.h
+//  TRx
+//
+//  Created by Mark Bellott on 4/5/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HQTextField : UITextField
+
+@end
