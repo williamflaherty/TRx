31623aa4accde04e91509811df2c996c9acf3fe5
John Cotham <jcotham@utk.edu>
2013-04-13 13:29:40 -0400

    addedLocalLoad

---
+++ TRx/LocalLoad.h
@@ -0,0 +1,13 @@
+//
+//  LocalLoad.h
+//  TRx
+//
+//  Created by John Cotham on 4/9/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import "localtalk.h"
+
+@interface LocalLoad : LocalTalk
+
+@end
---
+++ TRx/LocalLoad.m
@@ -0,0 +1,19 @@
+//
+//  LocalLoad.m
+//  TRx
+//
+//  Created by John Cotham on 4/9/13.
+//  Copyright (c) 2013 Team Ecuador. All rights reserved.
+//
+
+#import "LocalLoad.h"
+
+@implementation LocalLoad
+
+
++(NSString *)loadFromTableIntoLocal:(NSString *)table {
+    
+}
+
+
+@end
